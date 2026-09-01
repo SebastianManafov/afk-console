d43dfd7dc6749936f6398e630ef00d04519d5c81
SebastianManafov <SebastianManafov@users.noreply.github.com>
2026-09-01 07:16:59 +0200

    Refresh cached POV viewer after updates

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'rcc-v1'
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/styles.css','/app.js']))))
+const CACHE = 'rcc-v2'
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/styles.css','/app.js'])).then(() => self.skipWaiting())))
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())))
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET' || new URL(event.request.url).pathname.startsWith('/api/')) return
